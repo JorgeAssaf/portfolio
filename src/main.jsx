93bd308a3111b7dfa05f774b87a3ad55fc090be6
JorgeAssaf <jorgeassaf160@gmail.com>
2022-09-22 15:51:25 -0500

    refactor: 💡 Refactor files with Prettier
    BREAKING CHANGE: 🧨 no

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
 import { render } from 'preact'
 import { App } from './app'
 import './index.css'
-// import AOS from "aos";
-// import "aos/dist/aos.css";
 
-// AOS.init({
-//   delay: 150,
-// });
-render(
-  <App />, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      'sm': '250px',
-      'md': '700px',
-      'lg': '1000px',
-      'ult': '2540px',
+      sm: '250px',
+      md: '700px',
+      lg: '1000px',
+      ult: '2540px',
       '5k': '5000px',
     },
     extend: {},
