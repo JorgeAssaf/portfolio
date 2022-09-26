b119fa7c623b3e2f25ba8f89f34684f693d2d833
JorgeAssaf <jorgeassaf160@gmail.com>
2022-09-26 16:13:25 -0500

    refactor: 💡 No semicolon
    BREAKING CHANGE: 🧨 no

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export { default as Contact } from './Contact';
-export { default as Footer } from './Footer';
-export { default as HeroBanner } from './HeroBanner';
-export { default as Navbar } from './Navbar';
-export { default as Proyects } from './Proyects';
-export { default as Technologies } from './Technologies';
+export { default as Contact } from './Contact'
+export { default as Footer } from './Footer'
+export { default as HeroBanner } from './HeroBanner'
+export { default as Navbar } from './Navbar'
+export { default as Projects } from './Projects'
+export { default as Technologies } from './Technologies'
